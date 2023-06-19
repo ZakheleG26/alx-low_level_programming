3727cc2d0268f4ab569120ac94282ed2e1e8a70b
Zakhele Gwamanda <gwamandazakhele026@gmail.com>
2023-06-19 07:49:44 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio>
+/**
+ * main - Enter
+ * Return: Everytime 0 (Successful)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
