c998db7f36b602e701aa9340bf09ac81f0751e2e
Zakhele Gwamanda <gwamandazakhele026@gmail.com>
2023-06-19 06:57:48 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Enter
+ * Return: Everytime 0 (Successful)
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'z') {
+			putchar (d);
+			d++;
+		}
+	putchar ('\n');
+	return (0);
+}
