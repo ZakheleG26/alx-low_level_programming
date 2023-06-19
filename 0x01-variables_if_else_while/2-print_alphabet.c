30b540022217d34b673990f6bea3ed7576f19a21
Zakhele Gwamanda <gwamandazakhele026@gmail.com>
2023-06-19 06:37:09 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio>
+/**
+ * main - Enter
+ * Return: Everytime 0 (Successful)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('n\');
+	return (0);
+}
