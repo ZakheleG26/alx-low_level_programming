9d011602d588bb3a9cb8f0cb8870327720b2e537
Zakhele Gwamanda <gwamandazakhele026@gmail.com>
2023-06-19 07:57:10 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Enter
+ * Return: Everytime 0 (Successful)
+ */
+
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
