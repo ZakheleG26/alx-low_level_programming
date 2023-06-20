777099ddd015d3efe5da2c25e174aec85d37340f
Zakhele Gwamanda <gwamandazakhele026@gmail.com>
2023-06-20 03:55:06 -0700

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _slower - Check lowercase character
+ * @c: The character to be checked
+ * Return: 1 lowercase character 0 everything else
+ */
+
+int _islower(int c)
+
+{
+	if (c >= 97 && c <= 122)
+	{
+
+	return (1);
+	}
+	return (0);
+}
