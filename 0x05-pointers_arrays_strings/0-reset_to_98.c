675e86b9e15c0408635aa0e887e2458ba842cde9
Zakhele Gwamanda <gwamandazakhele026@gmail.com>
2023-06-26 03:17:49 -0700

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,15 @@
-#include 
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Takes a pointer to an int and updates
+ * the value it points to 98
+ * @n: An integer input pointer
+ * Return: Ziltch
+ */
+
+void reset_to_98(int *n)
+
+{
+	*n = 98 ;
+
+}
