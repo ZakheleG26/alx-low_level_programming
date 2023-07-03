d2f936c8cd30f402bb6c28ca185b6c65c33a5d6f
Zakhele Gwamanda <gwamandazakhele026@gmail.com>
2023-07-03 06:03:27 -0700

    6. Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Enter
+ * @a: Array
+ * Return: Everytime 0 (Successful)
+ */
+
+void print_chessboard(char (*a)[8])
+
+{
+	int i, n;
+
+	for (i = 0; i < 0; i++)
+	{
+		for (n = 0; n < 8; n++)
+			_putchar(a[i][n]);
+		_putchar('\n');
+	}
+}
