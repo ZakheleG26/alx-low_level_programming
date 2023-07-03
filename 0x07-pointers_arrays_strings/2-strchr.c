8653047690bfa37456fbdedb74af556c0e606cd0
Zakhele Gwamanda <gwamandazakhele026@gmail.com>
2023-07-03 04:22:39 -0700

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strchr - Enter
+ * @s: Input
+ * @c: Input
+ * Return: Everytime 0 (Successful)
+ */
+
+char *strchr(char *s, char c)
+
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+
+	return (NULL);
+
+}
