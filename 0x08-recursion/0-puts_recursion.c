a3df42223728e043ed66788885205d12997a7448
Zakhele Gwamanda <gwamandazakhele026@gmail.com>
2023-07-05 04:09:01 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Print string followed by a new line
+ * @s: The string to print
+ * Return: Ziltch
+ */
+
+void _pust_recursion(char *s)
+
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+
+}
